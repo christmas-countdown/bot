60058d46e541eb20fa643b665cfdcb4306ce0132
Isaac (eartharoid) <contact@eartharoid.me>
2020-09-25 21:27:11 +0100

    Replace moment with spacetime

--- src/modules/christmas.js
+++ src/modules/christmas.js
@@ -5,39 +5,39 @@
  * @license MIT
  */
 
-const moment = require('moment-timezone');
+const spacetime = require('spacetime');
 
 const lengths = [
 	['seconds', 1000, 1],
 	['minutes', 60, 60],
 	['hours', 60, 3600],
 	['days', 24, 86400],
-];
+]; // the order of these is actually important 
 
 module.exports = class ChristmasCountdown {
 	constructor(timezone) {
 		this.timezone = timezone || 'UTC';
 
-		this.now = moment();
+		this.now = spacetime.now(timezone);
 
 		let year = this.now.year();
 
-		if (this.now.month === 11 && this.now.date > 24)
+		if (this.now.month() === 11 && this.now.date() > 24)
 			year++; // if it's already Christmas, set date to next Christmas
 		
-		this.christmas = moment.tz(year + '-12-25 00:00', this.timezone);
+		this.christmas = spacetime(`December 25, ${year} 0:00:00`, this.timezone);
 	}
 
 	get months() {
-		return this.christmas.diff(this.now, 'months');
+		return this.now.diff(this.christmas, 'months');
 	}
 
 	get weeks() {
-		return this.christmas.diff(this.now, 'weeks');
+		return this.now.diff(this.christmas, 'weeks');
 	}
 
 	get days() {
-		return this.christmas.diff(this.now, 'days');
+		return this.now.diff(this.christmas, 'days');
 	}
 
 	get sleeps() {
@@ -45,27 +45,28 @@ module.exports = class ChristmasCountdown {
 	}
 
 	get hours() {
-		return this.christmas.diff(this.now, 'hours');
+		return this.now.diff(this.christmas, 'hours');
 	}
 
 	get minutes() {
-		return this.christmas.diff(this.now, 'minutes');
+		return this.now.diff(this.christmas, 'minutes');
 	}
 
 	get seconds() {
-		return this.christmas.diff(this.now, 'seconds');
+		return this.now.diff(this.christmas, 'seconds');
 	}
 
 	get live() {
-		let live = {};
-		// let diff = this.christmas.unix() - this.now.unix();
-	
+		let live = {
+			month_based: this.christmas.since(this.now).diff,
+			day_based: {},
+		};
+
 		for (let i = 0; i < lengths.length; i++)
-			// live[lengths[i][0]] = Math.floor(diff / lengths[i][2]) % (i === lengths.length - 1 ? 7 : lengths[i + 1][1]);
 			if (i === lengths.length - 1)
-				live[lengths[i][0]] = this[lengths[i][0]];
+				live.day_based[lengths[i][0]] = this[lengths[i][0]];
 			else
-				live[lengths[i][0]] = this[lengths[i][0]] - (this[lengths[i + 1][0]] * lengths[i + 1][1]);
+				live.day_based[lengths[i][0]] = this[lengths[i][0]] - (this[lengths[i + 1][0]] * lengths[i + 1][1]);
 
 		return live;
 	}
