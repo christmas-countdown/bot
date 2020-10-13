071586b12ea1a6cd850b5a72f123e022071bf6d1
Isaac (eartharoid) <contact@eartharoid.me>
2020-10-13 23:27:07 +0100

    Update christmas.js

--- src/modules/christmas.js
+++ src/modules/christmas.js
@@ -30,6 +30,27 @@ module.exports = class ChristmasCountdown {
 			year++; // if it's already Christmas, set date to next Christmas
 
 		this.christmas = spacetime(`December 25, ${year} 0:00:00`, this.timezone); // midnight on Christmas day
+
+		// for (let i = 1; i <= 31; i++)
+		// 	console.log(
+		// 		'October',
+		// 		i,
+		// 		spacetime(`October ${i}, 2020 0:00:00`, this.timezone).diff(this.christmas, 'days')
+		// 	);
+
+		// for (let i = 1; i <= 30; i++)
+		// 	console.log(
+		// 		'November',
+		// 		i,
+		// 		spacetime(`November ${i}, 2020 0:00:00`, this.timezone).diff(this.christmas, 'days')
+		// 	);
+
+		// for (let i = 1; i <= 25; i++)
+		// 	console.log(
+		// 		'December',
+		// 		i,
+		// 		spacetime(`December ${i}, 2020 0:00:00`, this.timezone).diff(this.christmas, 'days')
+		// 	);
 	}
 
 	get weeks() {
@@ -53,12 +74,28 @@ module.exports = class ChristmasCountdown {
 			months_based: this.christmas.since(this.now).diff,
 			days_based: {},
 		};
+		let n = this.now.diff(this.christmas, 'seconds');
+		console.log(n);
+		let days = n / (24 * 3600) ;
 
-		for (let i = 0; i < lengths.length; i++)
-			if (i === lengths.length - 1)
-				live.days_based[lengths[i][0]] = this[lengths[i][0]];
-			else
-				live.days_based[lengths[i][0]] = this[lengths[i][0]] - (this[lengths[i + 1][0]] * lengths[i + 1][1]);
+		n = n % (24 * 3600);
+		let hours = n / 3600;
+
+		n %= 3600;
+		let minutes = n / 60;
+
+		n %= 60;
+		let seconds = n;
+
+		live.days_based = {
+			days, hours, minutes, seconds
+		};
+
+		// for (let i = 0; i < lengths.length; i++)
+		// 	if (i === lengths.length - 1)
+		// 		live.days_based[lengths[i][0]] = this[lengths[i][0]];
+		// 	else
+		// 		live.days_based[lengths[i][0]] = this[lengths[i][0]] - (this[lengths[i + 1][0]] * lengths[i + 1][1]);
 
 		return live;
 	}
