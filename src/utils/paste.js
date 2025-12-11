2f02a8f832312a7dee1f20dc71d45baa2085d61f
Isaac <git@eartharoid.me>
2025-12-11 01:11:19 +0000

    fix paste

--- src/utils/paste.js
+++ src/utils/paste.js
@@ -1,6 +1,5 @@
-const fetch = require('node-fetch');
 module.exports = async function paste(body, type = 'plain') {
-	const res = fetch('https://api.pastes.dev/post', {
+	const res = await fetch('https://api.pastes.dev/post', {
 		body,
 		headers: {
 			'Content-Type': `text/${type}`,
